17dfb0d30b5955d0a99055748ce995aff14d8178
bastien <bastien>
2022-04-03 11:51:14 +0200

    Add location exact filtering on fields

--- locations/views.py
+++ locations/views.py
@@ -46,10 +46,26 @@ class LocationViewSet(viewsets.ModelViewSet):
         queryset = Location.objects.all()
 
         """ Values filtering """
-        # id filtering
+        # Part string search filtering in city OR country
         search_string = self.request.GET.get('s')
         if search_string is not None:
             queryset = queryset.filter(city__icontains=search_string) | queryset.filter(country__icontains=search_string)
+        # base location city filtering (nested object)
+        city = self.request.GET.get('city')
+        if city is not None:
+            queryset = queryset.filter(city=city)
+        # base location country filtering (nested object)
+        country = self.request.GET.get('country')
+        if country is not None:
+            queryset = queryset.filter(country=country)
+        # base location latitude filtering (nested object)
+        latitude = self.request.GET.get('latitude')
+        if latitude is not None:
+            queryset = queryset.filter(latitude=latitude)
+        # base location longitude filtering (nested object)
+        longitude = self.request.GET.get('longitude')
+        if longitude is not None:
+            queryset = queryset.filter(longitude=longitude)
 
         return queryset
 
@@ -61,6 +77,22 @@ class LocationViewSet(viewsets.ModelViewSet):
             openapi.Parameter('s',
             openapi.IN_QUERY,
             description='Allows part-match filtering on first_name OR last_name (both results combined, ex: ?s=Cabar).',
+            type=openapi.TYPE_STRING),
+            openapi.Parameter('city',
+            openapi.IN_QUERY,
+            description='Allows exact match filtering on city (Ex: ?city=Cabarete).',
+            type=openapi.TYPE_STRING),
+            openapi.Parameter('country',
+            openapi.IN_QUERY,
+            description='Allows exact match filtering on country (Ex: ?country=Dominican Republic).',
+            type=openapi.TYPE_STRING),
+            openapi.Parameter('longitude',
+            openapi.IN_QUERY,
+            description='Allows exact match filtering on longitude (Ex: ?longitude=19.0).',
+            type=openapi.TYPE_STRING),
+            openapi.Parameter('latitude',
+            openapi.IN_QUERY,
+            description='Allows exact match filtering on latitude (Ex: ?latitude=-70.0).',
             type=openapi.TYPE_STRING),
         ],
     )
